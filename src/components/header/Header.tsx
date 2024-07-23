99501385168e7091f4700b60241b60ca90c0542c
Vinícius Gonzato <vinigonzatofw@gmail.com>
2024-07-23 01:28:41 -0300

    feat: add header and pages Home, Contact, Shop

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import user from "../../assets/images/icons/user.svg";
+import card from "../../assets/images/icons/card.svg";
+import logotype from "../../assets/images/logotype.svg";
+
+const Header = () => {
+  return (
+    <header className="bg-white flex items-center justify-between py-7 px-24 flex-col md:flex-row gap-3 md:gap-0">
+      <div className="flex gap-1 items-center">
+        <img src={logotype} alt="Logotype" className="w-[50px] h-8" />
+        <h1 className="font-bold font-montserrat text-[34px]">Furniro</h1>
+      </div>
+      <nav>
+        <ul className="flex gap-12 md:gap-[75px] font-medium font-poppins text-base">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex gap-9">
+        <Link to="#">
+          <img src={user} alt="User" />
+        </Link>
+        <Link to="#">
+          <img src={card} alt="Card" />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
