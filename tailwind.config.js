17e65ee22098f8f20030123fbf84bbce0754b196
Vinícius Gonzato <vinigonzatofw@gmail.com>
2024-07-23 09:20:59 -0300

    feat: Add custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customYellow: "#FFF3E3",
+        customGold: "#B88E2F",
+        customGray: "#9F9F9F",
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+        montserrat: ["Montserrat"],
+      },
+    },
   },
   plugins: [],
 };
